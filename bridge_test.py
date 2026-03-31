f05ca30c1e882a6962c643212b88ba7e49d0ea5a
Ronnie <121251352+roniepris@users.noreply.github.com>
2026-03-31 08:53:24 -0300

    Update bridge_test.py

--- bridge_test.py
+++ bridge_test.py
@@ -1,1 +1,30 @@
+from bridge import TV, Radio, ControleRemoto, ControleAvancado
 
+def test_tv_ligar(capsys):
+    tv = TV()
+    controle = ControleRemoto(tv)
+
+    controle.ligar()
+
+    captured = capsys.readouterr()
+    assert "TV ligada" in captured.out
+
+
+def test_radio_volume(capsys):
+    radio = Radio()
+    controle = ControleAvancado(radio)
+
+    controle.aumentar_volume()
+
+    captured = capsys.readouterr()
+    assert "Volume do Rádio: 20" in captured.out
+
+
+def test_desligar_tv(capsys):
+    tv = TV()
+    controle = ControleRemoto(tv)
+
+    controle.desligar()
+
+    captured = capsys.readouterr()
+    assert "TV desligada" in captured.out
